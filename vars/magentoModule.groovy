991c301a69fa5b8e8d7beb58b8834dff554204b1
Pierre Dahlström <pierre.dahlstrom@vaimo.com>
2016-12-06 10:19:18 +0100

    Initial commit

---
+++ vars/magentoModule.groovy
@@ -0,0 +1,13 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    // now build, based on the configuration provided
+    node {
+        sh "echo Test"
+        sh "echo name: ${config.name}"
+    }
+}
