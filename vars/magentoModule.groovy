f0c06a4b9b1133d7a63fbe550aee0f9f6508f4c6
Pierre Dahlström <pierre.dahlstrom@vaimo.com>
2016-12-07 10:00:03 +0100

    Add Composer validate

--- vars/magentoModule.groovy
+++ vars/magentoModule.groovy
@@ -4,6 +4,10 @@ def call() {
     }
     
     stage('Code check') {
+        stage('Validate Composer') {
+            sh "composer validate"
+        }
+
         stage('PHP Lint') {
             sh "find . -name '*.php' -print0 | xargs -0 -n1 -P10 php -l -ddisplay_errors=on"
         }
