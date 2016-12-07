08cf4667320d694becc93b7b9ae5593543bb1d36
Pierre Dahlström <pierre.dahlstrom@vaimo.com>
2016-12-07 09:50:54 +0100

    Add PHP lint

--- vars/magentoModule.groovy
+++ vars/magentoModule.groovy
@@ -1,17 +1,11 @@
-def call(Closure body) {
-    node {
-        stage('Generate Composer Package') {
-            build 'generate composer package'
-        }
-        
-        stage('General build step') {
-            sh "echo Build!"
-            sh "pwd"
-            sh "printenv"
-        }
-        
-        stage('Job specific build step') {
-            body()
+def call() {
+    stage('Generate Composer Package') {
+        build 'generate composer package'
+    }
+    
+    stage('Code check') {
+        stage('PHP Lint') {
+            sh "find . -name '*.php' -print0 | xargs -0 -n1 -P10 php -l -ddisplay_errors=on"
         }
     }
 }
